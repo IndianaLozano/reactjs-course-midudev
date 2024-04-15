30be6f0eb13a926926d1fb9b78014e786c28fd9f
indilozano19 <indilozano19@gmail.com>
2024-04-15 09:29:57 -0300

    feat: WIP Twitter follow card

---
+++ projects/01-twitter-follow-card/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export function App() {
+
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/gravatar/sindresorhus@gmail.com"/>
+                <div>
+                    <strong>Indiana Lozano</strong>
+                    <span>@indiana.lozano</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Follow</button>
+            </aside>
+        </article>
+    )
+}
+
